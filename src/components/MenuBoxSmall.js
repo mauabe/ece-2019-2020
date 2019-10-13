88b4562e37e194ff60010510dc4a7e237d5ee713
Mauricio Feldman-Abe <6901736+mauabe@users.noreply.github.com>
2019-10-13 15:27:53 -0700

    fixed header images and replaced with better resolution

--- src/components/MenuBoxSmall.js
+++ src/components/MenuBoxSmall.js
@@ -6,7 +6,7 @@ import React from 'react';
 const MenuBoxSmall = props => {
   // console.log('%c PROPS at MenuBoxSmall', 'color:black;background:magenta;padding:6px;border:1px dashed black', props)
   return (
-    <div className="menu-box-small">
+    <div className="menu-box-">
       <h5 className="menu-item">{props.menuItem}</h5>
     </div>
   );
